62a2ed8367668552a73247f72e95fcf984e306e9
Vitor Spader <vitor.h.spader@gmail.com>
2024-10-18 22:23:22 -0300

    Adicionado modulo sqlite

--- src/ingestion/main.py
+++ src/ingestion/main.py
@@ -1,52 +1,9 @@
-from typing import Union
 from datetime import date
 
 from fastapi import FastAPI
-
-from pydantic import BaseModel
+from model.IngestionModels import Order, Customer, OrderItem, Product, User, Store
 
 app = FastAPI()
-
-
-class Order(BaseModel):
-    id: str
-    customer_id: str
-    date: date
-    value : float
-    is_canceled: bool
-    status: str
-    seller_id: str
-    store_id: str
-
-class Customer(BaseModel):
-    id: str
-    name: str
-    description: str
-    mobile_phone: str
-    phone: str
-    email: str
-
-class OrderItem(BaseModel):
-    id: str
-    order_id: str
-    product_id: str
-    value: float
-
-class Product(BaseModel):
-    id: str
-    name: str
-    description: str
-    group: str
-
-class Store(BaseModel):
-    id: str
-    name: str
-    parent_store: str
-
-class User(BaseModel):
-    id: str
-    name: str
-
 
 @app.post("/Order")
 def recv_order(order_data: list[Order]):
