991299d67c99d3ae0b0516b10cf763823cce7ff9
Vitor Spader <vitor.h.spader@gmail.com>
2024-10-21 22:02:27 -0300

    Created upsert with sqlite

--- src/ingestion/model/Sqlite.py
+++ src/ingestion/model/Sqlite.py
@@ -13,74 +13,39 @@ class SQLite(IDatabase):
         self.database = database
         SQLite.CONNECTION = sqlite3.connect(database=self.database)
     
-    def upsert(self, table:str, data:pd.DataFrame)->bool:
-        sql = f"INSERT INTO {table} VALUES('"
-        
-        columns = [column for column in data]
-        
-        df = sql + data[columns].agg("','".join, axis=1) + "')"
-        
-        cursor = self.CONNECTION.cursor()
-        for insert in df:
-            cursor.execute(insert)
+    def insert(self, table:str, data:pd.DataFrame):
+        self.__write(table, data, False)
 
-    def delete(ids:list[str])->bool:
-        pass
+    def upsert(self, table:str, data:pd.DataFrame, is_upsert:bool):
+        self.__write(table, data, True)
 
-    def append(self, table:str, data:pd.DataFrame)->bool:
-        sql = f"INSERT INTO {table} VALUES('"
-        
-        columns = [column for column in data]
-        
-        df = sql + data[columns].agg("','".join, axis=1) + "')"
-        
-        cursor = self.CONNECTION.cursor()
-        for insert in df:
-            cursor.execute(insert)
+    def delete(self, table:str, ids:list[str]):
 
-    def write(self, table:str, data:any):
-        print([x for x in data])
-        try:
-            if isinstance(data, list):
-                self._write_list(data, SQLite.CONNECTION.cursor(), table)
-            elif isinstance(data, pd.DataFrame):
-                self._write_df(data, table)
-            SQLite.CONNECTION.commit()
-        except Exception as e:
-            raise e
+        self.CONNECTION.rollback()
 
-    def _write_df(self, data:pd.DataFrame, table:str):
-        sql = f"INSERT INTO {table} VALUES('"
+        sql = f"DELETE FROM {table} WHERE id in ({ids.join(',')})"
 
-        columns = [column for column in data]
+        self.CONNECTION.cursor().execute(sql)
+        self.CONNECTION.commit()
+
         
-        df = sql + data[columns].agg("','".join, axis=1) + "')"
-        
-        cursor = self.CONNECTION.cursor()
-        for insert in df:
-            cursor.execute(insert)
+    def __write(self, table:str, data:pd.DataFrame, is_upsert:bool):
 
-    def _write_list(data:list, cursor:sqlite3.Connection, table:str):
-        for item in data:
-            sql = f"INSERT INTO {table} VALUES((select ifnull(max(idBidding),0)+1 from Bidding),'{"','".join(item)}')"
-            print(sql)
-            try:
-                print(cursor.execute(sql))
-                SQLite.CONNECTION.commit()
-            except sqlite3.Error as er:
-                print('SQLite error: %s' % (' '.join(er.args)))
-                print("Exception class is: ", er.__class__)
-                print('SQLite traceback: ')
-                exc_type, exc_value, exc_tb = sys.exc_info()
-                print(traceback.format_exception(exc_type, exc_value, exc_tb))
-            except Exception as e:
-                raise e
-        return
-    
-    def search(self, _query:str) -> any:
-        return SQLite.CONNECTION.execute(_query).fetchall() 
-    
-    def get_table_columns(self, table:str) -> List[str]:
-        cursor = self.CONNECTION.cursor()
-        cursor.execute(f'select * from {table} limit 1')
-        return [i[0] for i in cursor.description]
+        self.CONNECTION.rollback()
+
+        columns = [col for col in data.columns if col != 'id']
+
+        if is_upsert:
+            update_set_clause = ", ".join([f"{col} = excluded.{col}" for col in columns])
+        
+
+        for _, row in data.iterrows():
+            
+            sql = f"INSERT INTO {table} (id, {', '.join(columns)}) VALUES (?, {', '.join(['?'] * len(columns))})"
+            
+            if is_upsert:
+                sql = sql + f" ON CONFLICT(id) DO UPDATE SET {update_set_clause};"
+
+            values = [row['id']] + [row[col] for col in columns]
+            self.CONNECTION.cursor().execute(sql, values)
+            self.CONNECTION.commit()
